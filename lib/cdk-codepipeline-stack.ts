68d7946c1e3ba44fd6901f5e83e64791e2948070
Lopez, Sebastian (AgileEngine) <Lopez_Sebastian@ne.bah.com>
2023-05-19 16:18:28 -0300

    Test

--- lib/cdk-codepipeline-stack.ts
+++ lib/cdk-codepipeline-stack.ts
@@ -10,7 +10,10 @@ export class CdkCodepipelineStack extends cdk.Stack {
     // The code that defines your stack goes here
     const cicdpipeline = new CodePipeline(this, 'cicdpipeline',{
       synth: new ShellStep('Synth', {
-        input: CodePipelineSource.gitHub('xewax22/cdk-codepipeline','main'),
+        input: CodePipelineSource.gitHub('xewax22/cdk-codepipeline','main',{
+          // This is optional
+          authentication: cdk.SecretValue.secretsManager('github-token'),
+        }),
         commands: ['npm ci','npm run build','npx cdk synth'],
       }),
     } )
